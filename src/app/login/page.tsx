7422990ca85ea71f3b02b15b67c9ba886d5efca9
Tobias Lebeitsuk <tobias.lebeitsuk@gmail.com>
2025-10-07 09:47:39 +0300

    Hannes-Loginpage-wip

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 export default function LoginPage() {
-    return (
-        <div></div>
-    )
+	return (
+		<div className='bg-gray-400 h-svh flex justify-center items-center'>
+			<form action="" className="flex flex-col gap-2.5">
+				<label htmlFor="username" className="">Username</label>
+				<input type="text" name="username" className='border border-black' />
+				<label htmlFor="password">Password</label>
+				<input type="password" name="password" className='border border-black' />
+				<button type='submit' className='bg-white mt-2'>Login</button>
+			</form>
+		</div>
+	)
 }
